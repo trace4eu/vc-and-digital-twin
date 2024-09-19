c5a07b89733523fea7b9155327b76ca4ec6f6573
sid030sid <78762408+sid030sid@users.noreply.github.com>
2024-09-19 15:21:29 +0200

    set up swagger api

--- packages/issuer/src/main.ts
+++ packages/issuer/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Verifiable Credential Issuer')
+    .setDescription('With this API EBSI compliant VCs can be issued following OID4VCI protocol')
+    .setVersion('1.0')
+    .addTag('OID4VCI')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
